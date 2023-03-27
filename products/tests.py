88a91ad94065c4d4a9c3eab1fd85e072eaa6512e
Shved15 <maks.shihiwtsow15@gmail.com>
2023-03-27 16:43:41 +0200

    added tests, caching(redis) and pending tasks(celery)

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,53 @@
-from django.test import TestCase
+from http import HTTPStatus
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+
+from products.models import Product, ProductCategory
+
+
+class IndexViewTestCase(TestCase):
+
+    def test_view(self):
+        path = reverse('index')
+        response = self.client.get(path)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context_data['title'], 'Hockey - Shop')
+        self.assertTemplateUsed(response, 'products/index.html')
+
+
+class ProductsListViewTestCase(TestCase):
+    fixtures = ['goods_categories.json', 'goods.json']
+
+    def setUp(self):
+        self.products = Product.objects.all()
+
+    def test_list_products(self):
+        path = reverse('products:index')
+        response = self.client.get(path)
+
+        # print(response.context_data['object_list'])
+        # print(products[:3])
+        # print(list(response.context_data['object_list']) == list(products[:3]))
+        # self.assertEqual(response.status_code, HTTPStatus.OK)
+        # self.assertEqual(response.context_data['title'], 'Shop - Catalog')
+        # self.assertTemplateUsed(response, 'products/products.html')
+        self._common_tests(response)
+        self.assertEqual(list(response.context_data['object_list']), list(self.products[:3]))
+
+    def test_list_categories(self):
+        category = ProductCategory.objects.first()
+        path = reverse('products:category', kwargs={'category_id': 1})
+        response = self.client.get(path)
+
+        self._common_tests(response)
+        self.assertEqual(
+            list(response.context_data['object_list']),
+            list(self.products.filter(category_id=category.id))
+        )
+
+    def _common_tests(self, response):
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context_data['title'], 'Shop - Catalog')
+        self.assertTemplateUsed(response, 'products/products.html')
--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,55 @@
-from django.test import TestCase
+from datetime import timedelta
+from http import HTTPStatus
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+from django.utils.timezone import now
+
+from users.models import EmailVerification, User
+
+
+class UserRegistrationViewTestCase(TestCase):
+
+    def setUp(self):
+        self.data = {
+            'first_name': 'John',
+            'last_name': 'Brezek',
+            'username': 'johnbrezek',
+            'email': 'johnbrezek@gmail.com',
+            'password1': 'Password.1',
+            'password2': 'Password.1',
+        }
+        self.path = reverse('users:registration')
+
+    def test_user_registration_get(self):
+        response = self.client.get(self.path)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(response.context_data['title'], 'Shop - Registration')
+        self.assertTemplateUsed(response, 'users/registration.html')
+
+    def test_user_registration_post_success(self):
+        username = self.data['username']
+        # check the user does not exist
+        self.assertFalse(User.objects.filter(username=username).exists())
+        response = self.client.post(self.path, self.data)
+
+        # check creating of user
+        self.assertEqual(response.status_code, HTTPStatus.FOUND)
+        self.assertRedirects(response, reverse('users:login'))
+        self.assertTrue(User.objects.filter(username=username).exists())
+
+        # check email verification
+        email_verification = EmailVerification.objects.filter(user__username=username)
+        self.assertTrue(email_verification.exists())
+        self.assertEqual(
+            email_verification.first().expiration.date(),
+            (now() + timedelta(hours=48)).date()
+        )
+
+    def test_user_registration_post_error(self):
+        user = User.objects.create(username=self.data['username'])
+        response = self.client.post(self.path, self.data)
+
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertContains(response, 'A user with that username already exists.', html=True)
