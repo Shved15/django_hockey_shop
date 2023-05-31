7bdad5f0058299de9e2fb4ed163c98fd8c7944ed
Shved15 <maks.shihiwtsow15@gmail.com>
2023-05-31 17:15:37 +0200

    one more change for static

--- shop/settings.py
+++ shop/settings.py
@@ -195,7 +195,7 @@ if DEBUG:
         BASE_DIR / 'static',
     ]
 else:
-    STATIC_ROOT = BASE_DIR / 'static'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
